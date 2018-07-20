80508c84bb36629f2c2f45a58525255ea3bb5dd3
Sodian <188007086@qq.com>
2018-07-20 12:46:44 +0000

    Admin can manage site settings

--- config/administrator.php
+++ config/administrator.php
@@ -47,8 +47,10 @@ return array(
             'topics',
             'replies',
         ],
+        '站点管理' => [
+            'settings.site',
+        ],
     ],
-
     /*
      * 权限控制的回调函数。
      *
