02a4b45e6b0eb7da3d124336d316aeb573de728c
Sodian <188007086@qq.com>
2018-09-22 13:45:39 +0000

    user update

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,14 @@ $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
+
+                // 编辑登录用户信息
+$api->patch('user', 'UsersController@update')
+    ->name('api.user.update');
+                 // 图片资源
+    $api->post('images', 'ImagesController@store')
+        ->name('api.images.store');
+
         });
 
     });
