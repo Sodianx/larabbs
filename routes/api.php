a77cc226cb625465dbd34c6351c25da2f4bcab14
Sodian <188007086@qq.com>
2018-09-22 14:32:46 +0000

    topics store

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,10 @@ $api->patch('user', 'UsersController@update')
                  // 图片资源
     $api->post('images', 'ImagesController@store')
         ->name('api.images.store');
+        // 发布话题
+$api->post('topics', 'TopicsController@store')
+    ->name('api.topics.store');
+
 
         });
 
